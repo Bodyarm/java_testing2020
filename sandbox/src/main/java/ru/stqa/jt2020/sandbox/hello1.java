d7cd8e4a5080c46dd88f5c5c64327ba40abfc8b2
Bodyarm <i.v.gen@yandex.ru>
2020-08-07 23:39:25 +0300

    Добавлены первые функции

--- sandbox/src/main/java/ru/stqa/jt2020/sandbox/hello1.java
+++ sandbox/src/main/java/ru/stqa/jt2020/sandbox/hello1.java
@@ -2,7 +2,32 @@ package ru.stqa.jt2020.sandbox;
 
 public class hello1 {
 
- public static void main(String[] args) {
-	System.out.println("Hello, world!");
- } 
+    public static void main(String[] args) {
+        hello("Karlos");
+        hello("Jane");
+
+        System.out.println(2 / 2);
+        System.out.println(1.0 / 2.0);
+        int storona = 5;
+        int sq =storona * storona;
+        System.out.println("Площадь квадрата со стороной "+storona+ "=" + sq);
+        double lne = 5;
+        System.out.println("Площадь через функцию = "+ area(lne));
+        System.out.println("Площать прямоугольника ="+ area(7,3));
+
+
+    }
+
+    public static void hello(String somebody){
+
+        System.out.println("Hello, world! " + somebody);
+    }
+
+    public static double area(double l){
+        return l*l;
+    }
+    public static double area(double a,double b){
+        return a*b;
+    }
 }
+
