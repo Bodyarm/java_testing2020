1de84818e26bb72d52212940eed1cb31513c8160
Bodyarm <i.v.gen@yandex.ru>
2020-10-13 05:12:07 +0300

    Fix *Class -> *Method

--- addressbook-web-tests/src/test/java/ru/stqa/giv/addressbook/ContractCreationsTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/giv/addressbook/ContractCreationsTests.java
@@ -9,7 +9,7 @@ public class ContractCreationsTests {
     private WebDriver wd;
 
 
-    @BeforeClass(alwaysRun = true)
+    @BeforeMethod(alwaysRun = true)
     public void setUp() throws Exception {
         wd = new FirefoxDriver();
         wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
@@ -72,7 +72,7 @@ public class ContractCreationsTests {
         wd.findElement(By.linkText("add new")).click();
     }
 
-    @AfterClass(alwaysRun = true)
+    @AfterMethod(alwaysRun = true)
     public void tearDown() throws Exception {
         wd.quit();
 
