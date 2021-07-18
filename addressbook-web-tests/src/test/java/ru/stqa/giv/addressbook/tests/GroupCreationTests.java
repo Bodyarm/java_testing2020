1eb848a7ec057cea9014cabbe9000061cf67e8a3
Bodyarm <i.v.gen@yandex.ru>
2021-07-18 19:00:02 +0300

    lection 4.8
    Доработали сравнение множеств в тесте на создание групп.

--- addressbook-web-tests/src/test/java/ru/stqa/giv/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/giv/addressbook/tests/GroupCreationTests.java
@@ -4,6 +4,8 @@ import org.testng.Assert;
 import org.testng.annotations.*;
 import ru.stqa.giv.addressbook.model.GroupData;
 
+import java.security.acl.Group;
+import java.util.HashSet;
 import java.util.List;
 
 public class GroupCreationTests extends TestBase {
@@ -15,10 +17,23 @@ public class GroupCreationTests extends TestBase {
         List<GroupData> before = app.getGroupHelper().getGroupList();
         System.out.println("Groups Amount is "+ before.size() );
 
-        app.getGroupHelper().createGroup(new GroupData("test1", null, null));
+        GroupData group = new GroupData("test1", null, null);
+        app.getGroupHelper().createGroup(group);
         List<GroupData> after = app.getGroupHelper().getGroupList();
         System.out.println("Groups Amount is "+ after.size() );
         Assert.assertEquals(after.size(),before.size()+1);
+
+        int max = 0;
+        for (GroupData g: after){
+            if (g.getId()>max){
+                max = g.getId();
+            }
+        }
+        group.setId(max);
+
+        before.add(group);
+
+        Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
 
     }
 
