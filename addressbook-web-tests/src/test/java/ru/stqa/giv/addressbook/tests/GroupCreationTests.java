c2a5b0bb53c6ceb3e5d461f31ace482b13f077d3
Bodyarm <i.v.gen@yandex.ru>
2021-07-18 19:13:41 +0300

    lection 4.9
    Вычисление maxId через лямбда-функцию

--- addressbook-web-tests/src/test/java/ru/stqa/giv/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/giv/addressbook/tests/GroupCreationTests.java
@@ -1,10 +1,9 @@
 package ru.stqa.giv.addressbook.tests;
 
 import org.testng.Assert;
-import org.testng.annotations.*;
+import org.testng.annotations.Test;
 import ru.stqa.giv.addressbook.model.GroupData;
 
-import java.security.acl.Group;
 import java.util.HashSet;
 import java.util.List;
 
@@ -17,22 +16,14 @@ public class GroupCreationTests extends TestBase {
         List<GroupData> before = app.getGroupHelper().getGroupList();
         System.out.println("Groups Amount is "+ before.size() );
 
-        GroupData group = new GroupData("test1", null, null);
+        GroupData group = new GroupData("testZ", null, null);
         app.getGroupHelper().createGroup(group);
         List<GroupData> after = app.getGroupHelper().getGroupList();
         System.out.println("Groups Amount is "+ after.size() );
         Assert.assertEquals(after.size(),before.size()+1);
 
-        int max = 0;
-        for (GroupData g: after){
-            if (g.getId()>max){
-                max = g.getId();
-            }
-        }
-        group.setId(max);
-
+        group.setId(after.stream().max((o1, o2) -> Integer.compare(o1.getId(),o2.getId())).get().getId());
         before.add(group);
-
         Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
 
     }
