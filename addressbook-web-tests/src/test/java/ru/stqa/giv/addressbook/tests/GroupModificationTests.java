6d6135c8471645a478b4ab7f5b2d1bcbb43c770d
Bodyarm <i.v.gen@yandex.ru>
2021-07-18 18:42:05 +0300

    lection 4.7
    Доработали тест на модификацию группы. Теперь сравниваем множества целиком

--- addressbook-web-tests/src/test/java/ru/stqa/giv/addressbook/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/giv/addressbook/tests/GroupModificationTests.java
@@ -4,6 +4,7 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.giv.addressbook.model.GroupData;
 
+import java.util.HashSet;
 import java.util.List;
 
 public class GroupModificationTests extends TestBase{
@@ -19,13 +20,19 @@ public class GroupModificationTests extends TestBase{
         System.out.println("Groups Amount is "+ before.size() );
         app.getGroupHelper().selectGroup(before.size() - 1);
         app.getGroupHelper().initGroupModification();
-        app.getGroupHelper().fillGroupForm(new GroupData("mod1","mod2", "mod3"));
+        GroupData group = new GroupData(before.get(before.size()-1).getId(),"mod1","mod2", "mod3");
+        app.getGroupHelper().fillGroupForm(group);
         app.getGroupHelper().submitGroupModification();
         app.getGroupHelper().returnToGroupPage();
 
         List<GroupData> after = app.getGroupHelper().getGroupList();
         System.out.println("Groups Amount is "+ after.size() );
         Assert.assertEquals(after.size(), before.size());
+
+        before.remove((before.size()-1));
+        before.add(group);
+        Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
+
 
     }
 }
