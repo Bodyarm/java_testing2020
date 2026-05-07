e97d644b42ac68735263452c8f911fa4432f4ce2
Bodyarm <i.v.gen@yandex.ru>
2026-05-08 00:23:47 +0200

    task 14

--- addressbook-web-tests/src/test/java/ru/stqa/giv/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/giv/addressbook/appmanager/ApplicationManager.java
@@ -6,11 +6,17 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.remote.Browser;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.time.Duration;
+import java.util.Properties;
 
 
 public class ApplicationManager {
 
+    private Properties properties;
     WebDriver wd;
     private GroupHelper groupHelper;
     private NavigationHelper navigationHelper;
@@ -18,23 +24,33 @@ public class ApplicationManager {
     private ContractHelper contracthelper;
     private String browser;
 
-    public ApplicationManager(String browser) {
+    public ApplicationManager(String browser) throws IOException {
         this.browser = browser;
+
     }
 
 
-    public void init() {
+    public void init() throws IOException {
+
+        String target =System.getProperty("target", "local");
+        File checkpath = new File(".");
+        System.out.println(checkpath.getAbsolutePath());
+        System.out.println(String.format("src/test/resources/%s.properties",target));
+        properties = new Properties();
+        properties.load(new FileReader(String.format("src/test/resources/%s.properties",target)));
+
+
              if (browser.equals(Browser.FIREFOX.browserName())) wd = new FirefoxDriver();
         else if (browser.equals(Browser.CHROME.browserName()))  wd = new ChromeDriver();
         else if (browser.equals(Browser.IE.browserName()))      wd = new InternetExplorerDriver();
 
         wd.manage().timeouts().implicitlyWait(Duration.ofSeconds(2));
-        wd.get("http://localhost/addressbook/");
+        wd.get(properties.getProperty("web.baseUrl"));
         groupHelper      = new GroupHelper(wd);
         navigationHelper = new NavigationHelper(wd);
         sessionHelper    = new SessionHelper(wd);
         contracthelper   = new ContractHelper(wd);
-        sessionHelper.login("admin", "secret");
+        sessionHelper.login(properties.getProperty("web.login"), properties.getProperty("web.password"));
     }
 
 
--- addressbook-web-tests/src/test/java/ru/stqa/giv/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/stqa/giv/addressbook/tests/TestBase.java
@@ -7,9 +7,19 @@ import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
 import ru.stqa.giv.addressbook.appmanager.ApplicationManager;
 
+import java.io.IOException;
+
 public class TestBase {
 
-    protected static final ApplicationManager app = new ApplicationManager(Browser.CHROME.browserName());
+    protected static final ApplicationManager app;
+
+    static {
+        try {
+            app = new ApplicationManager(System.getProperty("browser",Browser.CHROME.browserName()));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
     @BeforeSuite(alwaysRun = true)
     public void setUp() throws Exception {
